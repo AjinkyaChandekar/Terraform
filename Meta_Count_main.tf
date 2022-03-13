babbd794bf28f5f00d65d2f287cf8ee02b862080
AjinkyaChandekar <65499227+AjinkyaChandekar@users.noreply.github.com>
2022-03-13 05:32:18 +0530

    Update Meta_Count_main.tf

--- Meta_Count_main.tf
+++ Meta_Count_main.tf
@@ -1,20 +1,12 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
 provider "aws" {
   region = "us-east-1"
 }
 
 resource "aws_instance" "Server" {
-  ami           = "ami-0e472ba40eb589f49"
-  key_name = var.key_name
-  instance_type = var.instance_type
-  security_groups= [var.security_group]
+  ami             = "ami-0e472ba40eb589f49"
+  key_name        = var.key_name
+  instance_type   = var.instance_type
+  security_groups = [var.security_group]
 
   tags = {
     Name = "Server"
